4e1789f685f6768a3425c0c2c9054a0272498ce8
Alberto Huerta <albertohuerta@albertohuertamac.lan>
2022-05-06 01:39:52 -0400

    added swiper and sass files

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,29 +1,53 @@
 import React from 'react';
-import './Home.css';
+import './Home.scss';
 
-import OfferPhoto from '../../assets/offerPicture.jpg';
-import ClassesPhoto from '../../assets/gymClasses.jpg';
-import PersonalTrainer from '../../assets/personaltrainer2.0.jpg';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, EffectFade, Pagination } from 'swiper';
+
+import Pic1 from '../../assets/offerPicture.jpg';
+import Pic2 from '../../assets/gymClasses.jpg';
+import Pic3 from '../../assets/personaltrainer2.0.jpg';
 
 const Home = () => {
   return (
     <div className="home-container">
       <div className="offer-container">
-        <img className="offer-image" src={OfferPhoto} alt="gym"></img>
+        <img className="offer-image" src={Pic3} alt="gym"></img>
         <div className="search-box">Find A club Near You!</div>
         <div className="offer-content">Hurry Now! No Sign Up Fee!</div>
         <div className="offer-logo"></div>
       </div>
       <div className="classes-container">
-        <img src={ClassesPhoto} alt="classesPhoto"></img>
+        <img src={Pic3} alt="classesPhoto"></img>
         <div className="classes-logo"></div>
+        <Swiper
+          modules={[Navigation, EffectFade, Pagination]}
+          // autoHeight={true}
+          navigation
+          // effect={'fade'}
+          speed={800}
+          slidesPerView={1}
+          loop
+          className="myswiper"
+          pagination
+        >
+          <SwiperSlide className="swiperslide">
+            <img src={Pic1} alt="/" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={Pic2} alt="/" className="swiperslide" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={Pic3} alt="/" className="swiperslide" />
+          </SwiperSlide>
+        </Swiper>
         <div className="classes-content">
           <button>Browse Classes</button>
         </div>
         <div className="classes-popup"></div>
       </div>
       <div className="personal-training-container">
-        <img src={PersonalTrainer} alt="personaltrainer" />
+        <img src={Pic3} alt="personaltrainer" />
         <div className="personal-training-content">
           <div className="personal-training-logo"></div>
           <div className="personal-training-description"></div>
